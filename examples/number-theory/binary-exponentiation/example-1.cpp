c9b4bc677ef99fc487224a194e22a897da395eb2
Parth <partharora99160808@gmail.com>
2020-12-27 15:06:10 +0530

    Added binary exponentiation

---
+++ examples/number-theory/binary-exponentiation/example-1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#include "dragon/number-theory/binary-exponentiation.hpp"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+  long long a, b, m;
+  m = 3;
+  a = 4;
+  b = 5;
+  cout << dragon::binary_exponentiation(a, b, m) << endl;
+  cout << dragon::binary_exponentiation(a, b) << endl;
+}
---
+++ include/dragon/number-theory/binary-exponentiation.hpp
@@ -0,0 +1,32 @@
+#ifndef DRAGON_NUMBER_THEORY_BINARY_EXPONENTIATION_HPP
+#define DRAGON_NUMBER_THEORY_BINARY_EXPONENTIATION_HPP
+
+#include <bits/stdc++.h>
+namespace dragon {
+// Time complexity : O(lg(b))
+template <typename T>
+inline constexpr T binary_exponentiation(T a, T b, T m) noexcept {
+  T res = 1;
+  while (b) {
+    if (b & 1)
+      res = (res * a) % m;
+    a = (a * a) % m;
+    b >>= 1;
+  }
+  return res;
+}
+
+// Time complexity : O(lg(b))
+template <typename T>
+inline constexpr T binary_exponentiation(T a, T b) noexcept {
+  T res = 1;
+  while (b) {
+    if (b & 1)
+      res = (res * a);
+    a = (a * a);
+    b >>= 1;
+  }
+  return res;
+}
+} // namespace dragon
+#endif
