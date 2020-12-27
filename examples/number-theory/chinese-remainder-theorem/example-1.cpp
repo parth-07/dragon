14e51252381be9146d73bdfb9880c3887a854aa2
Parth <partharora99160808@gmail.com>
2020-12-27 15:08:13 +0530

    Added chinese remainder theorem

---
+++ examples/number-theory/chinese-remainder-theorem/example-1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include "dragon/number-theory/chinese-remainder-theorem.hpp"
+
+using namespace std;
+
+int main()
+{
+  vector<int> a{1,2,3};
+  vector<int> m{3,5,7};
+
+  cout<<dragon::solve_typical_crt_problem(a,m)<<"\n";
+}
---
+++ include/dragon/number-theory/chinese-remainder-theorem.hpp
@@ -0,0 +1,30 @@
+#ifndef DRAGON_NUMBER_THEORY_CHINESE_REMAINDER_THEOREM_HPP
+#define DRAGON_NUMBER_THEORY_CHINESE_REMAINDER_THEOREM_HPP
+#include "mod-inverse.hpp"
+#include <vector>
+
+namespace dragon {
+// Time complexity : O(a.size()*lg(*max(m.begin(),m.end())))
+// Time complexity : O(len(a)*max(m))
+// Numbers in m vector should be relatively prime to each other
+template <typename RandomAccessContainer>
+auto solve_typical_crt_problem(const RandomAccessContainer& a,
+                               const RandomAccessContainer& m) {
+  using ValueType = typename RandomAccessContainer::value_type;
+
+  ValueType all_m_product = 1;
+  for (auto item : m)
+    all_m_product *= item;
+  std::vector<ValueType> m_k;
+  for (auto item : m)
+    m_k.push_back(all_m_product / item);
+  ValueType res = 0;
+  for (unsigned i = 0; i < a.size(); ++i) {
+    res = (res +
+           (a[i] * m_k[i] * mod_inverse_using_extended_euclid(m_k[i], m[i]))) %
+          all_m_product;
+  }
+  return res;
+}
+} // namespace dragon
+#endif
