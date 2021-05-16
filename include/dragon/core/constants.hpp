64ddef7a164daf85b760d74ad12fc2cb0b50bdd1
Parth <partharora99160808@gmail.com>
2021-05-16 12:40:01 +0530

    Add dragon core constant and util file

--- include/dragon/core/constants.hpp
+++ include/dragon/core/constants.hpp
@@ -1,11 +1,11 @@
-#ifndef DRAGON_CONSTANTS_HPP
-#define DRAGON_CONSTANTS_HPP
+#ifndef DRAGON_CORE_CONSTANTS_HPP
+#define DRAGON_CORE_CONSTANTS_HPP
 #include <cstddef>
 
 namespace dragon {
 namespace details {
 
-constexpr std::size_t bits_in_bytes = 8;
+constexpr std::size_t bits_in_byte = 8;
 } // namespace details
 } // namespace dragon
 
--- include/dragon/core/utils.hpp
+++ include/dragon/core/utils.hpp
@@ -1,15 +1,15 @@
-#ifndef DRAGON_UTILS_HPP
-#define DRAGON_UTILS_HPP
-#include "dragon/constants.hpp"
+#ifndef DRAGON_CORE_UTILS_HPP
+#define DRAGON_CORE_UTILS_HPP
+#include "dragon/core/constants.hpp"
 
 namespace dragon {
 namespace details {
 template <typename T> constexpr int msb_pos(const T& val) {
-  constexpr size_t total_bits = sizeof(T) * bits_in_bytes;
+  constexpr size_t total_bits = sizeof(T) * bits_in_byte;
   constexpr T one = static_cast<T>(1);
 
   for (int i = total_bits - 1; i >= 0; --i) {
-    if (val && (one << i))
+    if (val & (one<<i))
       return i;
   }
   return -1;
