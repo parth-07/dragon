d8d57c541ba4de560ea489ef2af8a7906c22094f
Parth Arora <partharora99160808@gmail.com>
2021-05-17 16:37:12 +0530

    Add counting sort (#6)

---
+++ examples/sorting/counting-sort/example-2.cpp
@@ -0,0 +1,29 @@
+#include "dragon/sorting/counting-sort.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct Character {
+  std::string name;
+  int rank;
+};
+
+int main() {
+  std::vector<Character> characters = {{"Mikasa", 5},
+                                       {"Touka", 1},
+                                       {"Kugisaki", 7},
+                                       {"Kurisu", 4},
+                                       {"Christina", 3}};
+  std::cout << "Before sorting:\n";
+  for (auto character : characters) {
+    std::cout << character.name << ": " << character.rank << "\n";
+  }
+  std::cout << "\n";
+  dragon::counting_sort(
+      characters.begin(), characters.end(),
+      [](const Character& character) { return character.rank; });
+  std::cout << "After sorting\n";
+  for (auto character : characters) {
+    std::cout << character.name << ": " << character.rank << "\n";
+  }
+}
