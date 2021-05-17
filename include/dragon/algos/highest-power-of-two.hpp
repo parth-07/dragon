3e1c24d5f5efd25c51bf6568484d03d8bfaf14ac
Parth <partharora99160808@gmail.com>
2021-05-17 09:18:15 +0530

    Add calculate highest power of 2 algorithm

---
+++ include/dragon/algos/highest-power-of-two.hpp
@@ -0,0 +1,26 @@
+#ifndef DRAGON_ALGOS_HIGHEST_POWER_OF_TWO_HPP
+#define DRAGON_ALGOS_HIGHEST_POWER_OF_TWO_HPP
+
+#include "dragon/core/constants.hpp"
+#include <iostream>
+namespace dragon {
+
+/**
+ * @return highest power of 2 not greater than n
+ */
+template<typename T>
+T cal_highest_power_of_two(T num) {
+  std::size_t sz = (sizeof num)*details::bits_in_byte;
+  const T one = static_cast<T>(1);
+  if (num & (one<<(sz-1)))
+    return (one<<(sz-1));
+
+  size_t shift = 1;
+  while (shift < sz) {
+    num |= (num>>shift);
+    shift*=2;
+  }
+  return ((num+1)>>1);
+}
+} // namespace dragon
+#endif
